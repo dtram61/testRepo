b64447c1bb6c1cc7f5ed47f6acfc941e949c61c0
Duc Tram <dtram61@gmail.com>
2023-01-13 05:48:14 -0800

    Added initial files for the project.

---
+++ src/main/java/edu/greenriver/sdev/projectUnderVcs/ProjectUnderVcsApplication.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.projectUnderVcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUnderVcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectUnderVcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/greenriver/sdev/projectUnderVcs/ProjectUnderVcsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.projectUnderVcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectUnderVcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
